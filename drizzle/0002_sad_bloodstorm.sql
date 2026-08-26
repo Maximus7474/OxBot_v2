fe09853e87e6e305679675edb42b56e59362b1d7
Maximus7474 <the.maximus.7474@gmail.com>
2026-08-26 15:22:58 +0200

    fix(migrations): added default hashes relating to "mr beast crypt scam" images

--- drizzle/0002_sad_bloodstorm.sql
+++ drizzle/0002_sad_bloodstorm.sql
@@ -6,4 +6,9 @@ CREATE TABLE `scamImageHashes` (
 	FOREIGN KEY (`added_by`) REFERENCES `users`(`id`) ON UPDATE cascade ON DELETE restrict
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `scamImageHashes_hash_unique` ON `scamImageHashes` (`hash`);
+CREATE UNIQUE INDEX `scamImageHashes_hash_unique` ON `scamImageHashes` (`hash`);--> statement-breakpoint
+INSERT INTO `scamImageHashes` (id, hash, added_at, added_by) VALUES
+    (1, 'f46178787e303e32', 0, '1'),
+    (2, 'f8b0e0ce6b686b2c', 0, '1'),
+    (3, 'ff00689d1e0f1f0e', 0, '1'),
+    (4, '18bd1d0f0f4ee1e1', 0, '1');
